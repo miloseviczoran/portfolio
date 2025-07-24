cb507cc6f4f568f1ef614c315863ed9c9dc5acb3
Zoran <mag.milosevic.zoran@gmail.com>
2025-07-24 08:06:56 +0200

    Refactored Navbar and fixed mobile responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
   });
 
   return (
-    <div id="about" className="min-h-screen w-screen px-5">
+    <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase md:text-[10px] xl:text-[30px]">
           Welcome to Zentry
